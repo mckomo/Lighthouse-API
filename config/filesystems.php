e51ffcd02001028566c5eff748b84f65aa328354
Mckomo <mckomo@gmail.com>
2016-07-18 00:09:21 +0200

    Updated configs for laravel 5.2

--- config/filesystems.php
+++ config/filesystems.php
@@ -48,24 +48,6 @@ return [
             'root'   => storage_path().'/app',
         ],
 
-        's3' => [
-            'driver' => 's3',
-            'key'    => 'your-key',
-            'secret' => 'your-secret',
-            'region' => 'your-region',
-            'bucket' => 'your-bucket',
-        ],
-
-        'rackspace' => [
-            'driver'    => 'rackspace',
-            'username'  => 'your-username',
-            'key'       => 'your-key',
-            'container' => 'your-container',
-            'endpoint'  => 'https://identity.api.rackspacecloud.com/v2.0/',
-            'region'    => 'IAD',
-            'url_type'  => 'publicURL',
-        ],
-
     ],
 
 ];
