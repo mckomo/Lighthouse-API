b71bf87a7fe1058803b3e77bc34d7f3cddfba100
mckomo <mckomo@gmail.com>
2016-09-09 13:03:45 +0200

    Import command with cached service

--- app/Console/Commands/ImportTorrentsCommand.php
+++ app/Console/Commands/ImportTorrentsCommand.php
@@ -4,6 +4,7 @@ namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Cache;
+use Lighthouse\Common\ResultCodes;
 use Lighthouse\Core\ServiceInterface;
 use Lighthouse\Core\TorrentMapperInterface;
 use Lighthouse\Error;
@@ -74,13 +75,12 @@ class ImportTorrentsCommand extends Command
      *
      * @return void
      */
-    public function __construct(ServiceInterface $service, TorrentMapperInterface $mapper, Cache $cache)
+    public function __construct(ServiceInterface $service, TorrentMapperInterface $mapper)
     {
         parent::__construct();
 
         $this->service = $service;
         $this->mapper = $mapper;
-        $this->cache = $cache;
     }
 
     /**
@@ -93,17 +93,16 @@ class ImportTorrentsCommand extends Command
         $this->setupCounters();
 
         foreach ($this->openDataFile() as $line) {
+
             $torrent = $this->mapTorrent($line);
 
-            if (is_null($torrent) || $this->isCached($torrent)) {
+            if (is_null($torrent)) {
                 continue;
             }
 
             $result = $this->service->put($torrent);
 
-            if ($result->isSuccessful()) {
-                $this->cache($torrent);
-            } else {
+            if ($result->isFailed()) {
                 $this->printError($result->getError());
             }
 
@@ -159,9 +158,11 @@ class ImportTorrentsCommand extends Command
     {
         $this->totalSaveCount++;
 
-        if ($result->isSuccessful()) {
+        if ($result->getCode() == ResultCodes::ResourceCreated) {
             $this->successfulSaveCount++;
-        } else {
+        }
+
+        if ($result->isFailed()) {
             $this->failedSaveCount++;
         }
     }
@@ -180,43 +181,8 @@ class ImportTorrentsCommand extends Command
         $this->info($statsMessage);
     }
 
-    private function hasTorrentChanged(Torrent $torrent)
-    {
-        $currentHash = $this->calculateEntityHash($torrent);
-        $storedHash = $this->getStoredHash($torrent);
-
-        return $currentHash != $storedHash;
-    }
-
     /**
-     * @param $torrent
-     *
-     * @return bool
-     */
-    private function isCached($torrent)
-    {
-        return is_null($torrent) and !$this->hasTorrentChanged($torrent);
-    }
-
-    private function cache(Torrent $torrent)
-    {
-        $entityHash = $this->calculateEntityHash($torrent);
-
-        return Cache::put($torrent->infoHash, $entityHash);
-    }
-
-    private function calculateEntityHash(Torrent $torrent)
-    {
-        return md5(json_encode($torrent->toArray()));
-    }
-
-    private function getStoredHash($torrent)
-    {
-        return Cache::get($torrent->hash);
-    }
-
-    /**
-     * @param FailedResult $result
+     * @param Error $result
      *
      * @return void
      */
