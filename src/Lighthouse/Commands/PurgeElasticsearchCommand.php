31f2b26d17ebc5a8a4807b98bb2d3ad411edb31c
mckomo <mckomo@gmail.com>
2016-09-29 10:57:58 +0200

    Starting with purge command implementations

---
+++ src/Lighthouse/Commands/PurgeElasticsearchCommand.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Lighthouse\Commands;
+
+use Elastica\Index;
+use Elastica\Type;
+use Elastica\Type\Mapping;
+
+class PurgeElasticsearchCommand
+{
+    /**
+     * @var Index
+     */
+    private $elastic;
+
+    /**
+     * @var string
+     */
+    private $options;
+
+    /**
+     * @param Index $elastic
+     */
+    public function __construct(Index $index)
+    {
+        $this->index = $index;
+    }
+
+    /**
+     * @return void
+     */
+    public function handle()
+    {
+        echo 'PURGING !';
+    }
+}
---
+++ src/Lighthouse/Commands/PurgeRedisCommand.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Lighthouse\Commands;
+
+use Elastica\Index;
+use Elastica\Type;
+use Elastica\Type\Mapping;
+use Predis\Client;
+
+class PurgeRedisCommand
+{
+    /**
+     * @var Client
+     */
+    private $redis;
+
+    /**
+     * @param Client $redis
+     */
+    public function __construct(Client $redis)
+    {
+        $this->redis = $redis;
+    }
+
+    /**
+     * @return void
+     */
+    public function handle()
+    {
+        echo 'PURGING !';
+    }
+}
