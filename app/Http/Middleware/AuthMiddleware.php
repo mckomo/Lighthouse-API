954aee990af9b02fe48bc74a015f68285a868da6
Maciej Komorowski <mckomo@gmail.com>
2017-03-12 01:37:19 +0100

    Add custom auth middleware

---
+++ app/Http/Middleware/AuthMiddleware.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+final class AuthMiddleware
+{
+    const UnauthorizedErrorMessage =  ['error' => ['message' => 'Unauthorized']];
+
+    /**
+     * Handle an incoming request.
+     *
+     * @param  Request  $request
+     * @param  Closure  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        if ($request->isMethodSafe(false)) {
+            return $next($request);
+        }
+
+        if ($this->containsValidAccessToken($request)) {
+            return $next($request);
+        }
+
+        return response(self::UnauthorizedErrorMessage, 401);
+    }
+
+    public function containsValidAccessToken(Request $request)
+    {
+        $accessToken = $request->input('access_token');
+
+        return $accessToken === env('APP_ACCESS_TOKEN', !$accessToken);
+    }
+}
